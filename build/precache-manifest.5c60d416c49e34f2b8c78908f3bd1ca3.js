51b60d4ca47ba7a9db57a39d21855c6b56605e48
Artur Zahorski <artur.zahorski@jmr-tech.local>
2019-06-04 16:47:20 +0200

    Create production build for GitHub pages

---
+++ build/precache-manifest.5c60d416c49e34f2b8c78908f3bd1ca3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81e7658ea6ae9808212bb344e3176a94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfd2e049f7042dd437a3",
+    "url": "/static/css/2.33bbdc9a.chunk.css"
+  },
+  {
+    "revision": "63034b02ece7afb1aa1f",
+    "url": "/static/css/main.46e65095.chunk.css"
+  },
+  {
+    "revision": "cfd2e049f7042dd437a3",
+    "url": "/static/js/2.69b24cfd.chunk.js"
+  },
+  {
+    "revision": "63034b02ece7afb1aa1f",
+    "url": "/static/js/main.bdb7cef5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
